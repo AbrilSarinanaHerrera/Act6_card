5aadec7a4ebf441982eb9d18ebcb9f48eedd9d25
User_T7 <a.20308051280553@cbtis128.edu.mx>
2023-04-20 22:14:41 +0000

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +32,66 @@ class MyHomePage extends StatelessWidget {
         // The title text which will be shown on the action bar
         title: Text(title),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Card(
+        child: Container(
+          height: 100,
+          color: Colors.white,
+          child: Row(
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Expanded(
+                    child: const Image(
+                        image: AssetImage('assets/images/logo8.png')),
+                    flex: 2,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: ListTile(
+                          title: Text("Floreria"),
+                          subtitle: Text("Arreglo 1"),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 5,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              child: Text("Comprar"),
+                              onPressed: () {},
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            TextButton(
+                              child: Text("Agregar a carrito"),
+                              onPressed: () {},
+                            ),
+                            SizedBox(
+                              width: 8,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                flex: 8,
+              ),
+            ],
+          ),
         ),
+        elevation: 8,
+        margin: EdgeInsets.all(10),
       ),
     );
   }
